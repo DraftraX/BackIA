d08e779304bfbf6281240df7e04acbf37d8ca55c
DraftraX <wmanuelga@gmail.com>
2025-07-11 17:54:39 -0500

    ruta root /

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from custom_auth.views import CustomLoginView
 
+from django.http import JsonResponse
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("seguridad.urls")),
@@ -10,4 +12,10 @@ urlpatterns = [
     path('api/auth/login/', CustomLoginView.as_view(), name='login_view'),
     path('api/encriptador/', include('encriptador.urls')),
     path('api/captcha/', include('captcha_verify.urls')),
+
+    # Ruta raíz (para evitar 404 en "/")
+    path('', lambda request: JsonResponse({
+        "status": "ok",
+        "message": "Bienvenido al backend de NIDS"
+    })),
 ]
